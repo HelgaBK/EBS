b77f0f86463f96c4103ce62eb8e61a8e9ce580ff
HelgaBK <olga.karpyshyn@gmail.com>
2020-11-19 17:19:41 +0200

    added DashBoard & MainMenu

--- meter_solutions_front/src/App.vue
+++ meter_solutions_front/src/App.vue
@@ -6,10 +6,16 @@
 
 <script>
 
+  import MainMenu from "@/components/MainMenu";
+  import DashBoard from "@/components/DashBoard";
 
 export default {
   name: 'App',
 
+  components: {
+    MainMenu,
+    DashBoard,
+  }
 }
 </script>
 
---
+++ meter_solutions_front/src/components/DashBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-container>
+    <el-aside width="auto">
+      <SideMenu v-bind:user="user"/>
+    </el-aside>
+    <el-container>
+      <Board v-bind:deviceId = "deviceId"/>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import SideMenu from "@/components/SideMenu";
+import Board from "@/components/Board";
+
+export default {
+  name: "DashBoard",
+  components: {
+    SideMenu,
+    Board,
+  },
+  data() {
+    return {
+      deviceId: "1"
+    }
+  },
+  props: {
+    user: Object,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
