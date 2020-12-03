9d809c8cce9c3b96337ec6e5a88ac9f604f205d5
HelgaBK <olga.karpyshyn@gmail.com>
2020-12-03 17:36:29 +0200

    updated App.vue

--- meter_solutions_front/src/App.vue
+++ meter_solutions_front/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-
+    <el-header>
+      <MainMenu/>
+    </el-header>
+    <el-main>
+      <DashBoard  v-bind:user="user"/>
+    </el-main>
   </div>
 </template>
 
@@ -11,14 +16,35 @@
 
 export default {
   name: 'App',
-
+  data() {
+    return {
+      user: {id: 1, email: 'test@test.test', flag: false},
+      token: null
+    }
+  },
   components: {
     MainMenu,
     DashBoard,
+  },
+  methods: {
+
   }
 }
 </script>
 
 <style>
+  @import url("//unpkg.com/element-ui@2.14.1/lib/theme-chalk/index.css");
 
+  html, body {
+    margin: 0px !important;
+    padding: 0px !important;
+    font-family: Avenir, Helvetica, Arial, sans-serif !important;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
 </style>
